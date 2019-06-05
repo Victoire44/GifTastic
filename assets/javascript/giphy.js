3717c9507da17546148db6584832512aa7383e07
Victoire <victoirebaron@hotmail.fr>
2019-06-04 20:09:09 -0700

    Ajax call to giphy

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,10 +3,32 @@ var topics = ["Game of Thrones", "Black Mirror", "The Crown", "House of card", "
 
 for (var i = 0; i < topics.length; i++) {
     var gifButton = $("<button>");
-    gifButton.addClass("btn btn-light")
+    gifButton.addClass("btn btn-danger")
     gifButton.text(topics[i])
 
-    $("#buttons").append(gifButton);    
+    $("#buttons").append(gifButton);
 }
 
+$("button").on("click", function () {
+    var series = $(this).text()
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        series + "&api_key=ya3czf6RnXKyCr1uNzKdX9hYTUlRtO5J&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var seriesDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var seriesImage = $("<img>");
+            seriesImage.attr("src", results[i].images.fixed_height.url);
+            seriesDiv.append(p);
+            seriesDiv.append(seriesImage);
+            $("#gifs-appear-here").prepend(seriesDiv);
+        }
+    })
+});
 
