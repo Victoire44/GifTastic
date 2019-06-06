4bab68acfce929c1500187e4d744e5f8d9b2e0bb
Victoire <victoirebaron@hotmail.fr>
2019-06-06 08:40:45 -0700

    creating inpput and button to add new series

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,4 +1,5 @@
-var topics = ["Game of Thrones", "Black Mirror", "The Crown", "House of card", "13 Reasons Why", "Stranger Things", "You", "Casa de Papel"];
+var topics = ["Game of Thrones", "Black Mirror", "The Crown", "House of card", "13 Reasons Why", "Stranger Things",
+    "Ozark", "La Casa de Papel", "Narcos", "Mindhunter", "Suburra"];
 
 
 for (var i = 0; i < topics.length; i++) {
@@ -24,11 +25,16 @@ $("button").on("click", function () {
             var seriesDiv = $("<div>");
             var p = $("<p>").text("Rating: " + results[i].rating);
             p.addClass("text-white");
+            p.css("margin", "10px")
             var stillImage = results[i].images.fixed_height_still.url;
             var animateImage = results[i].images.fixed_height.url;
             var seriesImage = $("<img>");
             seriesImage.attr("src", stillImage);
             seriesImage.attr("data-state", "still")
+            seriesImage.attr("data-animate", animateImage)
+            seriesImage.attr("data-still", stillImage)
+            seriesImage.css("margin", "10px")
+
             seriesDiv.append(p);
             seriesDiv.append(seriesImage);
 
@@ -37,14 +43,41 @@ $("button").on("click", function () {
             seriesImage.on("click", function () {
                 var state = $(this).attr("data-state")
                 if (state === "still") {
-                    $(this).attr("src", animateImage);
+                    $(this).attr("src", $(this).attr("data-animate"));
                     $(this).attr("data-state", "animate");
                 } else if (state === "animate") {
-                    $(this).attr("src", stillImage);
+                    $(this).attr("src", $(this).attr("data-still"));
                     $(this).attr("data-state", "still");
                 }
             });
+
         }
     })
 });
+
+function renderButtons() {
+    $("#buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-danger")
+        newButton.addClass("series-btn");
+        newButton.attr("data-name", topics[i]);
+        newButton.text(topics[i]);
+        $("#buttons").append(newButton);
+    }
+}
+
+$("#add-series").on("click", function (event) {
+    event.preventDefault();
+    var newSeries = $("#series-input").val().trim();
+    topics.push(newSeries);
+
+    renderButtons();
+});
+
+
+
+
+
+
 
